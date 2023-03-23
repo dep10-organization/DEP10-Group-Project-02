f2285f5079090bd7208471c2f75d7ee315002c6f
Sashin Sahasra <sashinsirichandra@gmail.com>
2023-03-23 19:56:59 +0530

    Implement loadCustomers()

--- src/main/java/lk/ijse/dep10/app/controller/CustomerViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/CustomerViewController.java
@@ -5,6 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import lk.ijse.dep10.app.db.DBConnection;
+import lk.ijse.dep10.app.model.Customer;
+
+import java.io.InputStream;
+import java.sql.*;
 
 public class CustomerViewController {
 
@@ -18,7 +26,7 @@ public class CustomerViewController {
     private Button btnSave;
 
     @FXML
-    private TableView<?> tblCustomer;
+    private TableView<Customer> tblCustomer;
 
     @FXML
     private TextField txtAddress;
@@ -28,6 +36,45 @@ public class CustomerViewController {
 
     @FXML
     private TextField txtName;
+
+    public void initialize() {
+        btnDelete.setDisable(true);
+
+        tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblCustomer.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCustomer.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        loadAllCustomers();
+
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((ov, prev, current) -> {
+            if (current == null) return;
+
+            btnDelete.setDisable(false);
+            txtId.setText(current.getId() + "");
+            txtName.setText(current.getName());
+            txtName.setText(current.getAddress());
+        });
+    }
+
+    private void loadAllCustomers() {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+            while (rst.next()) {
+                int id = rst.getInt("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+
+                tblCustomer.getItems().add(new Customer(id, name, address));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
 
