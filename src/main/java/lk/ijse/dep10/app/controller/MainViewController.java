fe5f3526db1b01f19e609a6e231c366828c33052
Sashin Sahasra <sashinsirichandra@gmail.com>
2023-03-23 19:21:21 +0530

    Complete main view to customer view transition

--- src/main/java/lk/ijse/dep10/app/controller/MainViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/MainViewController.java
@@ -2,7 +2,13 @@ package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class MainViewController {
 
@@ -19,8 +25,9 @@ public class MainViewController {
     private Button btnManageTeachers;
 
     @FXML
-    void btnManageCustomersOnAction(ActionEvent event) {
-
+    void btnManageCustomersOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnManageCustomers.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CustomerView.fxml"))));
     }
 
     @FXML
