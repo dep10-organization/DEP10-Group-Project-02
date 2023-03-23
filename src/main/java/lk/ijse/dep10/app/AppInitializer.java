59a4e2da299c7547753d196dd757a4fe06f9d540
Sashin Sahasra <sashinsirichandra@gmail.com>
2023-03-23 18:11:20 +0530

    Complete dashboard UI

--- src/main/java/lk/ijse/dep10/app/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/app/AppInitializer.java
@@ -1,7 +1,11 @@
 package lk.ijse.dep10.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -10,7 +14,11 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainView.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Main View");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
